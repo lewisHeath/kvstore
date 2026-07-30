949cc6dee1ea4397b24f88df3a598e1fd7f9579d
Lewis Heath <email@lewisheath.co.uk>
2026-07-30 22:52:06 +0100

    tcp server that echos the stuff back

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lewisHeath/kvstore
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"strings"
+	"time"
+)
+
+var port string
+
+func main() {
+	flag.StringVar(&port, "port", "3000", "The port the server listens on")
+	flag.Parse()
+	fmt.Printf("Initialising TCP server on port %v\n", port)
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	if err != nil {
+		fmt.Printf("Error starting the TCP server: %v\n", err)
+		return
+	}
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("Error accepting a connection on listener %v port %v: %v\n", ln.Addr().Network(), ln.Addr().String(), err)
+			return
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Printf("Accepted TCP connection from %v\n", conn.RemoteAddr())
+	buffer := make([]byte, 1024) // 1024 byte slice buffer to read the data into
+	for {
+		conn.SetReadDeadline(time.Now().Add(30 * time.Second))
+		n, err := conn.Read(buffer)
+		if err != nil {
+			var netErr net.Error
+			if errors.Is(err, io.EOF) {
+				fmt.Printf("Client %v disconnected\n", conn.RemoteAddr())
+			} else if errors.As(err, &netErr) && netErr.Timeout() {
+				fmt.Printf("Client %v timed out\n", conn.RemoteAddr())
+			} else {
+				fmt.Printf("Error reading from the TCP connection %v %v\n", conn.RemoteAddr(), err)
+			}
+			return
+		}
+		// Output the data and echo back
+		data := buffer[:n]
+		fmt.Printf("Received data from TCP socket: %s\n", strings.TrimRight(string(data), "\r\n"))
+		_, err = conn.Write(data)
+		if err != nil {
+			fmt.Printf("Error writing to the TCP connection %v %v\n", conn.RemoteAddr(), err)
+			return
+		}
+	}
+}
